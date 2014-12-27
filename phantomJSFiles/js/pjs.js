be4ee2c1f2295c4d19ffb6d5285127494a79a3cf
luozhixiang <luozhixiang82@gmail.com>
2014-12-27 21:31:30 +0800

    pjs12

---
+++ phantomJSFiles/js/pjs.js
@@ -0,0 +1,54 @@
+var page = require('webpage').create();
+var system = require('system');
+
+var url = system.args[1];
+var uri = system.args[2];
+var imageName = system.args[3];
+var json = system.args[4];
+var reportType = system.args[5];
+var reportName = system.args[6];
+page.viewportSize = {
+	width : 1300,
+	height : 900
+};
+
+var fs = require('fs');
+var content = fs.read(json);
+var obj = JSON.parse(content);
+console.log(reportName + "cccc")
+page.open(url + uri, function() {
+	// Transactional : Transactional Mailing Report :<Report Name>
+	// Program :Lifecycle Program Report :<Reprort Name>
+	// Batch : Batch Mailing Report : <Report Name>
+	// BATCH|TRANSACTIONAL|PROGRAM
+	var title = "";
+	if (reportType == "BATCH") {
+		title = ("Batch : Batch Mailing Report :" + reportName);
+	}
+	if (reportType == "TRANSACTIONAL") {
+		title = ("Transactional : Transactional Mailing Report :" + reportName);
+	}
+	if (reportType == "PROGRAM") {
+		title = ("Program :Lifecycle Program Report :" + reportName);
+	}
+	obj.title = title;
+	obj.reportType = reportType;
+	page.includeJs(url + '/js/report.js', function() {
+		doReport(page, obj)
+		window.setTimeout(function() {
+			// console.log("--->go image")
+			page.render(imageName);
+			phantom.exit();
+
+		}, 3000);
+
+	});
+});
+
+function doReport(page, json) {
+	page.evaluate(function(json) {
+		showSummaryChartPart('day', json.items[0].data);
+		showBottomSummaryPart('day', json.items[0].summary, json.reportType);
+		$("#reportTitle").html(json.title);
+	}, json);
+}
